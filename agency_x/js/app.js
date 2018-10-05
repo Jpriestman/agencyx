eee7da18748e9ef50bd80d8300f0bc4df9ebcaa7
Jordan Priestman <jordanpriestman@Jordans-MacBook-Pro.local>
2018-10-05 14:40:07 -0400

    Added the lightbox

--- agency_x/js/app.js
+++ agency_x/js/app.js
@@ -1,68 +1,34 @@
 $(document).foundation()
 
-//variables, arrays and DOM elements
- 	var	imageBanner = document.querySelector("#houseImages"),
- 		sigils = document.querySelectorAll(".sigilContainer"),
- 		lightbox = document.querySelector('.lightbox'),
- 		closeLBox = lightbox.querySelector('.close-lightbox'),
- 		vidPlayer = lightbox.querySelector('video'),
- 		tagLine = document.querySelector('#tagLine'),
+//variables
 
- 		
- 		offSet = 600,
- 		houseName = 'stark';
- 		var currentSource;
- 		var theExt;
-
-		imageBanner.style.right = "0px",
-		caption = ['Winter is Coming', 'Ours is the Fury', 'Hear Me Roar!', 'Family, Duty, Honor', 'We Do Not Sow', 'As High As Honor'];
-	
-
+var lightbox = document.querySelector('.lightbox');
+var closeLightB = document.querySelector('.closeLightbox');
+var galleryThumb = document.querySelectorAll('.casestudy');
+var mainphoto = document.querySelector('#portfolioImage');
+var imageDesc = document.querySelector('#imageDesc');
+var count = 0;
 
 //functions
 
-function closeLightBox() {
-	//(2)complete this function
-	vidPlayer.currenTime=0; //rewindes video to start
-	lightbox.classList.remove('show-lightbox');
-	vidPlayer.pause();
+function openLBox(){
+    lightbox.classList.add('show-lightbox');
 }
 
-function moveBanner() {
-	imageBanner.style.right = (offSet * this.dataset.offset) + 'px';
-	//set house name to name of the second class on this item(image in this case)
-	houseName = this.className.split(' ')[1];
-
-	var pick = this.dataset.offset;
-	tagLine.innerHTML = caption[this.dataset.offset]; 
-	//this means the current object clicked on..internal reference for the thing that was just clicked
-	//split will take a long string and make it an array by splitting it on the character that is specified
-	//() means function... [] means array
-	
+function closeLbox(){
+    lightbox.classList.remove('show-lightbox');
 }
 
-function showHouseVideo() {
-	currentSource = vidPlayer.currentSrc;
-	theExt = currentSource.substr(currentSource.lastIndexOf("."));
-	vidPlayer.src = 'video/house-'+houseName+theExt;
-	lightbox.classList.add('show-lightbox');
-	vidPlayer.load();
-	vidPlayer.play();
+function showPortfolioImage(){
+    mainphoto.src = this.querySelector('img').src.replace('/img','');
+    imageDesc.innerHTML = this.querySelector('.projectDesc').innerHTML;
+    lightbox.classList.add('show-lightbox');
 }
 
+//event listeners
 
-
-//events and listeners
-imageBanner.addEventListener('transitionend', showHouseVideo, false);
-
-closeLBox.addEventListener('click', closeLightBox);
-
-for(var i = 0; i < sigils.length; i++) {
-	sigils[i].addEventListener('click', moveBanner, false);
+for(var i = 0; i < galleryThumb.length; i++) {
+    galleryThumb[i].addEventListener('click', showPortfolioImage, false);
 }
 
-//(3)add listener for end of transition , use 'transitioned' event to open the lightbox and start playig the selected video'
-//(4) when video finised playing close the lightbox
-
-
-//4 LINES OF CODE
+closeLightB.addEventListener('click', closeLbox, false);
